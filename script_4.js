5a1e6a87d069d2f45d767cfd6a66d441c43a691a
yeezynico <yeezy77680@gmail.com>
2024-02-29 14:39:48 +0100

    Bibliothécaire

--- script_4.js
+++ script_4.js
@@ -17,4 +17,37 @@ const entrepreneurs = [
     { first: 'Travis', last: 'Kalanick', year: 1976 },
     { first: 'Marc', last: 'Andreessen', year: 1971 },
     { first: 'Peter', last: 'Thiel', year: 1967 }
-  ];
+];
+
+
+//nés dans les années 70 
+let années70 = entrepreneurs.filter(
+    (e) => e.year > 1970 && e.year < 1980);
+console.log(années70);
+
+//prénom et le nom des entrepreneurs
+
+let fullname = entrepreneurs.map(entrepreneur => `${entrepreneur.first} ${entrepreneur.last}`);
+
+console.log(fullname);
+
+//age
+let age = entrepreneurs.map(e =>
+    `${e.first} ${e.last} a ${2024 - e.year} ans`);
+console.log(age);
+
+//Trie les entrepreneurs par ordre alphabétique du nom de famille.
+
+let trie = entrepreneurs.sort((a, b) => {
+    if (a.last < b.last) {
+        return -1;
+    }
+    if (a.last > b.last) {
+        return 1;
+    }
+    return 0;
+});
+
+console.log(trie);
+
+
